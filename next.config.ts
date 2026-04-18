6d48e836c1a038e0108286c94dd6acab12d48d31
adhamlotr <adhammurad2001@gmail.com>
2026-04-18 12:31:17 +0300

    empty main for PR base

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const config: NextConfig = {
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: false },
-};
-
-export default config;
